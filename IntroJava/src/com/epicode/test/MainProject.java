6980f9a13cc2d605c47fac19588e5f191e58ef9c
Giorgia <giorgia890@live.com>
2023-05-03 09:45:14 +0200

    commit1

---
+++ IntroJava/src/com/epicode/test/MainProject.java
@@ -0,0 +1,33 @@
+package com.epicode.test;
+
+public class MainProject {
+
+	public static void main(String[] args) {
+		
+         boolean b = true;
+         char c = 'f';
+         int i = 25;
+         long l = i;
+         double d = 25.3d;
+         // float 32 bit -> double 64bit
+         float f = 25.3f;
+         
+         //String str =new String();
+         String s = "Ciao";
+         s = "abc";
+         
+         final String ABC = "Costante!";
+         //ABC = "hahahaha": -> ERRORE
+         
+         System.out.println("Ciao a tutti");
+        
+         int arr [] = new int [10]; 
+        arr [0] = 25;
+        arr [8] =9;
+        char sett [] = {'L', 'M', 'M' };
+        
+        System.out.println("sett.length");
+        
+	}
+
+}
